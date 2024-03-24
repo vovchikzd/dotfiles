34711e267a4145c00d3ea18ae8d44661fbcdb3be
Vladimir Zdorenko <vovchikzd@gmail.com>
2024-03-24 13:03:53 +0300

    simple wrapper for std::print header

---
+++ print.hpp
@@ -0,0 +1,29 @@
+#ifndef MY_PRINT_WRAPPER__
+#define MY_PRINT_WRAPPER__
+
+#if (__cplusplus >= 202302L)
+
+#include <print>
+
+template <typename... Args>
+void print(std::format_string<Args...> str, Args&&... args) {
+  std::print(stdout, str, std::forward<Args>(args)...);
+}
+
+template <typename... Args>
+void eprint(std::format_string<Args...> str, Args&&... args) {
+  std::print(stderr, str, std::forward<Args>(args)...);
+}
+
+template <typename... Args>
+void println(std::format_string<Args...> str, Args&&... args) {
+  std::println(stdout, str, std::forward<Args>(args)...);
+}
+
+template <typename... Args>
+void eprintln(std::format_string<Args...> str, Args&&... args) {
+  std::println(stderr, str, std::forward<Args>(args)...);
+}
+
+#endif // __cplusplus
+#endif // MY_PRINT_WRAPPER__
