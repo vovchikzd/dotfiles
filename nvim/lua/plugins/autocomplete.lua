7d4098f3616dc929c3b19a1b0216ed6ef605fb20
Vladimir Zdorenko <vovchikzd@gmail.com>
2024-05-22 05:12:30 +0300

    add autopair config to cmp plugin

--- nvim/lua/plugins/autocomplete.lua
+++ nvim/lua/plugins/autocomplete.lua
@@ -52,6 +52,12 @@ local autocomplete = {
       }),
       matching = { disallow_symbol_nonprefix_matching = false }
     })
+
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    cmp.event:on(
+      "confirm_done",
+      cmp_autopairs.on_confirm_done()
+    )
   end,
 }
 
