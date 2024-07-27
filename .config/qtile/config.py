e2ae772ff1387944f1083ab4d77c30be2ee7a8a3
Vladimir Zdorenko <vovchikzd@gmail.com>
2024-07-27 21:33:37 +0300

    add dolphin to floating class

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -231,6 +231,7 @@ floating_layout = layout.Floating(
         Match(title="branchdialog"),  # gitk
         Match(title="pinentry"),  # GPG key password entry
         Match(wm_class="mpv"),
+        Match(wm_class="dolphin")
     ]
 )
 auto_fullscreen = True
