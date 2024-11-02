eb68671697a551bd795fec151b156cc446f7d061
Vladimir Zdorenko <vovchikzd@gmail.com>
2024-11-02 22:31:14 +0300

    fix keymap

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -11,10 +11,6 @@ vim.g.maplocalleader = " "
 keymap("n", "<leader>|", "<C-w>v", opts("Vertical split"))
 keymap("n", "<leader>\\", "<C-w>s", opts("Horizontal split"))
 keymap("n", "<leader>c", "<C-w>c", opts("Close split"))
-keymap("n", "<C-l>", "<C-w>l", opts("Move cursor to right split window"))
-keymap("n", "<C-h>", "<C-w>h", opts("Move cursor to left split window"))
-keymap("n", "<C-j>", "<C-w>j", opts("Move cursor to down split window"))
-keymap("n", "<C-k>", "<C-w>k", opts("Move cursor to up split window"))
 
 keymap("n", "<C-h>", "<C-w>h", opts("Go to left window"))
 keymap("n", "<C-j>", "<C-w>j", opts("Go to below window"))
@@ -26,15 +22,15 @@ keymap("n", "<esc>", "<cmd>nohlsearch<CR>", opts("Disable highlight after search
 keymap("v", "<", "<gv", opts("Remove indent and stay in visual mode"))
 keymap("v", ">", ">gv", opts("Add indent and stay in visual mode"))
 
-keymap("v", "J", "<cmd>m '>+1<CR>gv=gv", opts("Move visual blok(line) down"))
-keymap("v", "K", "<cmd>m '<-2<CR>gv=gv", opts("Move visual blok(line) up"))
+keymap("v", "J", ":m '>+1<CR>gv=gv", opts("Move visual blok(line) down"))
+keymap("v", "K", ":m '<-2<CR>gv=gv", opts("Move visual blok(line) up"))
 
 keymap("v", "$", "g_", opts("Select to end of line without newline character"))
 keymap("n", "U", "<C-r>", opts("Redo"))
 
 -- insert new line above/below without insert mode
-keymap("n", "<leader>o", "o<esc>", opts("Insert newline below and stay in normal mode")) -- conflict with tree
-keymap("n", "<leader>O", "O<esc>", opts("Insert newline above and stay in normal mode"))
+-- keymap("n", "<leader>o", "o<esc>", opts("Insert newline below and stay in normal mode")) -- conflict with tree
+-- keymap("n", "<leader>O", "O<esc>", opts("Insert newline above and stay in normal mode"))
 
 keymap("t", "<Esc>", "<C-\\><C-n>", opts("Go to normal mode from terminal mode"))
 keymap("n", "tt", "<cmd>terminal<CR>", opts("Go to terminal mode"))
