035d0e3fcd3349252b3703013d1d4ffd6e74af1d
Vladimir Zdorenko <vovchikzd@gmail.com>
2024-11-18 11:12:26 +0300

    add highlight on yank

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -22,6 +22,15 @@ vim.filetype.add({
   },
 })
 
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight_yank', {}),
+  desc = 'Hightlight selection on yank',
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 250 }
+  end,
+})
+
 local opts = {
   change_detection = {
     -- automatically check for config file changes and reload the ui
