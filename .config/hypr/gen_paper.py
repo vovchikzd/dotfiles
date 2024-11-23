9014447acc13ee16557fb6d16ce47fd2572479a5
Vladimir Zdorenko <vovchikzd@gmail.com>
2024-11-23 20:57:48 +0300

    rewrite random paper script on python

---
+++ .config/hypr/gen_paper.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import os, random, subprocess
+
+def main():
+    sConfigFile: str = "/home/vovchik/dotfiles/.config/hypr/hyprpaper.conf"
+    sPapersDir: str = "/home/vovchik/dotfiles/images/wallpapers"
+    saFilesPaths: list[str] = [f"{sPapersDir}/{sFileName}" for sFileName in os.listdir(sPapersDir)]
+    sFilePath: str = random.choice(saFilesPaths)
+    with open(sConfigFile, "w") as fConfigFile:
+        print(f"preload = {sFilePath}", file=fConfigFile)
+        print(f"wallpaper = , {sFilePath}", file=fConfigFile)
+    subprocess.Popen("/usr/bin/hyprpaper", start_new_session=True)
+
+
+if __name__ == "__main__":
+    main()
