3f3d633b1d3bcef3957d5e058f70f962858f518d
Vladimir Zdorenko <vovchikzd@gmail.com>
2024-11-25 19:28:33 +0300

    change align plugin to mini's version

--- .config/nvim/lua/plugins/align.lua
+++ .config/nvim/lua/plugins/align.lua
@@ -1,15 +1,63 @@
 local align = {
-  "Vonr/align.nvim",
+  'echasnovski/mini.align',
+  version = false,
   lazy = true,
   init = function()
-    local opts = function(description)
-      return { noremap = true, silent = true, desc = description}
-    end
-    local keymap = vim.keymap.set
-    keymap("x", "aa", function() require("align").align_to_char({length = 1}) end, opts("Align to 1 character"))
-    keymap("x", "ad", function() require("align").align_to_char({length = 2, preview = true}) end, opts("Align to 2 characters"))
-    keymap("x", "aw", function() require("align").align_to_string({regex = false, preview = true}) end, opts("Align to strign"))
-  end,
+    require("mini.align").setup()
+  end
 }
 
 return align
+
+-- default setup
+--[[
+{
+  -- Module mappings. Use `''` (empty string) to disable one.
+  mappings = {
+    start = 'ga',
+    start_with_preview = 'gA',
+  },
+
+  -- Modifiers changing alignment steps and/or options
+  modifiers = {
+    -- Main option modifiers
+    ['s'] = --<function: enter split pattern>,
+    ['j'] = --<function: choose justify side>,
+    ['m'] = --<function: enter merge delimiter>,
+
+    -- Modifiers adding pre-steps
+    ['f'] = --<function: filter parts by entering Lua expression>,
+    ['i'] = --<function: ignore some split matches>,
+    ['p'] = --<function: pair parts>,
+    ['t'] = --<function: trim parts>,
+
+    -- Delete some last pre-step
+    ['<BS>'] = --<function: delete some last pre-step>,
+
+    -- Special configurations for common splits
+    ['='] = --<function: enhanced setup for '='>,
+    [','] = --<function: enhanced setup for ','>,
+    [' '] = --<function: enhanced setup for ' '>,
+  },
+
+  -- Default options controlling alignment process
+  options = {
+    split_pattern = '',
+    justify_side = 'left',
+    merge_delimiter = '',
+  },
+
+  -- Default steps performing alignment (if `nil`, default is used)
+  steps = {
+    pre_split = {},
+    split = nil,
+    pre_justify = {},
+    justify = nil,
+    pre_merge = {},
+    merge = nil,
+  },
+
+  -- Whether to disable showing non-error feedback
+  silent = false,
+}
+]]
