142701f9a4c20187892267c3c405a300855f4305
Vladimir Zdorenko <vovchikzd@gmail.com>
2025-03-23 09:46:09 +0300

    configure second monitor

--- .config/hypr/gen_paper.py
+++ .config/hypr/gen_paper.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 
 import os, random, subprocess
+from hashlib import md5
 
 def main():
     sConfigFile: str = "/home/vovchik/dotfiles/.config/hypr/hyprpaper.conf"
     sPapersDir: str = "/home/vovchik/dotfiles/images/wallpapers"
     saFilesPaths: list[str] = [f"{sPapersDir}/{sFileName}" for sFileName in os.listdir(sPapersDir)]
-    sFilePath: str = random.choice(saFilesPaths)
+    sFirstFile: str = random.choice(saFilesPaths)
+    sSeconFile: str = random.choice(saFilesPaths)
+    while (
+        md5(open(sFirstFile, 'rb').read()).hexdigest() ==
+            md5(open(sSeconFile, 'rb').read()).hexdigest()
+    ):
+        sSeconFile = random.choice(saFilesPaths)
     with open(sConfigFile, "w") as fConfigFile:
-        print(f"preload = {sFilePath}", file=fConfigFile)
-        print(f"wallpaper = , {sFilePath}", file=fConfigFile)
-    subprocess.Popen("/usr/bin/hyprpaper", start_new_session=True)
+        print(f"preload = {sFirstFile}", file=fConfigFile)
+        print(f"preload = {sSeconFile}", file=fConfigFile)
+        print(f"wallpaper = DP-2, {sFirstFile}", file=fConfigFile)
+        print(f"wallpaper = DP-3, {sSeconFile}", file=fConfigFile)
+    subprocess.Popen(
+        "/usr/bin/hyprpaper"
+        , start_new_session=True
+    )
 
 
 if __name__ == "__main__":
