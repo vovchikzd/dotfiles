6fc008e4ce5ab79e812b5c8c1d2dc995f24645e1
Vladimir Zdorenko <vovchikzd@gmail.com>
2025-03-23 20:59:50 +0300

    configure resizing in wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,5 +9,6 @@ config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 11
 config.window_close_confirmation = "NeverPrompt"
+config.adjust_window_size_when_changing_font_size = false
 
 return config
