ee75f66b8ef2582507d7964b55791020a7ce5fa1
Vladimir Zdorenko <vovchikzd@gmail.com>
2025-04-02 21:30:39 +0300

    add yazi to config

---
+++ .config/hypr/yazi_start.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+ACTIVE_WM_CLASS=$(hyprctl activewindow | grep 'class:')
+if [[ "$ACTIVE_WM_CLASS" == *"wezterm"* ]]; then
+  PID=$(hyprctl activewindow | grep 'pid:' | cut -d' ' -f2)
+  if [[ "$PID" == "" ]]; then
+    /usr/bin/wezterm start --always-new-process --class yazi -e yazi &
+  else
+    CHILD_PID=$(pgrep -P $PID)
+    if [[ "$CHILD_PID" == "" ]]; then
+      /usr/bin/wezterm start --always-new-process --class yazi -e yazi &
+    else
+      pushd "/proc/${CHILD_PID}/cwd" &>/dev/null
+      SHELL_CWD=$(/usr/bin/pwd -P)
+      popd &>/dev/null
+      printf "$SHELL_CWD" > '/home/vovchik/dotfiles/.config/hypr/debug.log'
+      /usr/bin/wezterm start --always-new-process --class yazi -e yazi "$SHELL_CWD" &
+    fi
+  fi
+else
+  /usr/bin/wezterm start --always-new-process --class yazi -e yazi &
+fi
