0de76b8169a87cb9c915708300dfec8079be2ccc
Vladimir Zdorenko <vovchikzd@gmail.com>
2025-04-11 07:22:01 +0300

    rewrite to typst

--- list/sort.py
+++ list/sort.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys
+import sys, inspect
 
 def get_tuple(line: str) -> tuple[str, int]:
     tup: list[str] = line.split(" ~ ")
@@ -12,14 +12,30 @@ def main() -> int:
         sorted_tmp = [get_tuple(line.strip()) for line in tmp.readlines()]
 
     sorted_tmp.sort(key=lambda tup: (tup[1], tup[0]))
-    with open("list.tex", "w") as tord:
-        print("\\documentclass[a4paper, 11pt]{proc} % proc для двух колонок\n\\input{preamble.tex}\n", file=tord)
-        print("\\begin{document}\n\n\\begin{enumerate}", file=tord)
+    with open("list.typ", "w") as outFile:
+        print(inspect.cleandoc(
+        """#set text(
+             size: 8.5pt
+             , font: "Hack Nerd Font"
+           )
+           #set page(margin: (
+             top: 5mm
+             , bottom: 5mm
+             , right: 5mm
+             , left: 5mm
+           ))
+           #set par(
+             leading: 0.7em
+             , spacing: 1.5pt
+           )
+
+           #columns(2, gutter: 0pt)[
+        """), file=outFile)
         for line in sorted_tmp:
-            print(f"    \\item {line[0]} -- {line[1]}", file=tord)
-            if ("(" not in line[0] or ")" not in line[0]):
+            print(f"+ {line[0]} -- {line[1]}", file=outFile)
+            if ('(' not in line[0] or ')' not in line[0]):
                 print(f"\033[0;33m{line}\033[0m")
-        print("\\end{enumerate}\n\n\\end{document}", file=tord)
+        print("]", file=outFile)
     return 0
 
 
