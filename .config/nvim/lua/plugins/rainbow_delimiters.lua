c1196dc70a5416aa2bf68c484ed41b3c0cd400b0
Vladimir Zdorenko <vovchikzd@gmail.com>
2025-04-18 05:58:16 +0300

    add rainbow delimiters plugin

---
+++ .config/nvim/lua/plugins/rainbow_delimiters.lua
@@ -0,0 +1,31 @@
+local rainbow_delimiters = {
+  "HiPhish/rainbow-delimiters.nvim"
+  , lazy = true
+  , init = function()
+    require('rainbow-delimiters.setup').setup({
+      strategy = {
+        [''] = 'rainbow-delimiters.strategy.global',
+        vim = 'rainbow-delimiters.strategy.local',
+      },
+      query = {
+        [''] = 'rainbow-delimiters',
+        lua = 'rainbow-blocks',
+      },
+      priority = {
+        [''] = 110,
+        lua = 210,
+      },
+      highlight = {
+        'RainbowDelimiterRed',
+        'RainbowDelimiterYellow',
+        'RainbowDelimiterBlue',
+        'RainbowDelimiterOrange',
+        'RainbowDelimiterGreen',
+        'RainbowDelimiterViolet',
+        'RainbowDelimiterCyan',
+      },
+    })
+  end
+}
+
+return rainbow_delimiters
