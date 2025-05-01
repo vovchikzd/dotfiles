22687c8c5a8eb42755aeed953134deaeadb20358
Vladimir Zdorenko <vovchikzd@gmail.com>
2025-05-01 13:44:08 +0300

    too lazy to write message

--- .config/hypr/yazi_start.sh
+++ .config/hypr/yazi_start.sh
@@ -4,18 +4,18 @@ ACTIVE_WM_CLASS=$(hyprctl activewindow | grep 'class:')
 if [[ "$ACTIVE_WM_CLASS" == *"wezterm"* ]]; then
   PID=$(hyprctl activewindow | grep 'pid:' | cut -d' ' -f2)
   if [[ "$PID" == "" ]]; then
-    /usr/bin/wezterm start --always-new-process --class yazi -e yazi &
+    /usr/bin/wezterm start --always-new-process --class yazi -e /home/vovchik/.cargo/bin/yazi &
   else
     CHILD_PID=$(pgrep -P $PID)
     if [[ "$CHILD_PID" == "" ]]; then
-      /usr/bin/wezterm start --always-new-process --class yazi -e yazi &
+      /usr/bin/wezterm start --always-new-process --class yazi -e /home/vovchik/.cargo/bin/yazi &
     else
       pushd "/proc/${CHILD_PID}/cwd" &>/dev/null
       SHELL_CWD=$(/usr/bin/pwd -P)
       popd &>/dev/null
-      /usr/bin/wezterm start --always-new-process --class yazi -e yazi "$SHELL_CWD" &
+      /usr/bin/wezterm start --always-new-process --class yazi -e /home/vovchik/.cargo/bin/yazi "$SHELL_CWD" &
     fi
   fi
 else
-  /usr/bin/wezterm start --always-new-process --class yazi -e yazi &
+  /usr/bin/wezterm start --always-new-process --class yazi -e /home/vovchik/.cargo/bin/yazi &
 fi
--- autostarts/desktop_portal.sh
+++ autostarts/desktop_portal.sh
@@ -2,7 +2,10 @@
 
 sleep 1
 killall -e xdg-desktop-portal-hyprland
+# killall xdg-desktop-portal-gtk
 killall xdg-desktop-portal
 /usr/lib/xdg-desktop-portal-hyprland &
 sleep 2
+# /usr/lib/xdg-desktop-portal-gtk &
+# sleep 2
 /usr/lib/xdg-desktop-portal &
