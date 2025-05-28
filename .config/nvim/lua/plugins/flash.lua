5aca7cc13203a46c0ba730ca64f42e07bcd1e55a
Vladimir Zdorenko <vovchikzd@gmail.com>
2025-05-28 05:43:50 +0300

    disable search mode

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -138,7 +138,7 @@ local opts = {
     search = {
       -- when `true`, flash will be activated during regular search by default.
       -- You can always toggle when searching with `require("flash").toggle()`
-      enabled = true,
+      enabled = false,
       highlight = { backdrop = false },
       jump = { history = true, register = true, nohlsearch = true },
       search = {
