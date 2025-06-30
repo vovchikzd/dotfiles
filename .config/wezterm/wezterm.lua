b90c284fd1e51c800fc23bc71330fef17621f2de
Vladimir Zdorenko <vovchikzd@gmail.com>
2025-06-30 17:58:23 +0300

    change font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,7 +6,8 @@ config.enable_wayland = false
 
 config.color_scheme = "Gruvbox dark, medium (base16)"
 config.hide_tab_bar_if_only_one_tab = true
-config.font = wezterm.font("Hack Nerd Font")
+-- config.font = wezterm.font("Hack Nerd Font")
+config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 11
 config.window_close_confirmation = "NeverPrompt"
 config.adjust_window_size_when_changing_font_size = false
