de3fa72ecfd24fa6e026921bb111f3267f7130b3
Vladimir Zdorenko <vovchikzd@gmail.com>
2025-07-11 05:02:38 +0300

    change signes

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -14,6 +14,15 @@ local opts = {
     changedelete = { text = '▁' },
     untracked    = { text = '┃' },
   },
+  signs_staged = {
+    add          = { text = '┃' },
+    change       = { text = '┃' },
+    delete       = { text = '▁' },
+    topdelete    = { text = '▁' },
+    changedelete = { text = '▁' },
+    untracked    = { text = '┃' },
+  },
+  signs_staged_enable = true,
   signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
   numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
   linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
