c75a70e0a699c895427266d9086b6adaf8a5564f
Vladimir Zdorenko <vovchikzd@gmail.com>
2025-07-21 08:30:18 +0300

    continue nvim configuration

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -7,7 +7,9 @@ return {
     require("nvim-treesitter.configs").setup({
       ensure_installed = { 'c', "cpp", "python", "lua", "bash", "rust", "cmake",
         "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore",
-        "llvm", "nasm", "ninja", "tmux", "markdown", "latex", "typst", "zig" }
+        "llvm", "nasm", "ninja", "tmux", "markdown", "latex", "typst", "zig",
+        "comment", "fish", "gnuplot", "hyprlang", "make", "markdown_inline",
+        "printf", "regex", "sql", "toml", "xml", "yaml" }
       , auto_install = true
       , ignore_install = {}
       , indent = { enable = true }
