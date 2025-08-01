1614eb33e5a3dcb01ae20295bc4af46670bd3420
Vladimir Zdorenko <vovchikzd@gmail.com>
2025-08-01 16:53:34 +0300

    little addition to compile config
    #
    # Commands:
    #   q          Close
    #   <c-c><c-c> Submit
    #   <c-c><c-k> Abort
    #   <m-p>      Previous Message
    #   <m-n>      Next Message
    #   <m-r>      Reset Message

--- .config/nvim/lua/plugins/probably/compile.lua
+++ .config/nvim/lua/plugins/probably/compile.lua
@@ -1,4 +1,12 @@
 return {
   "ej-shafran/compile-mode.nvim"
-  , dependencies = { "nvim-lua/plenary.nvim" }
+  , dependencies = {
+    "nvim-lua/plenary.nvim"
+    , { "m00qek/baleia.nvim", tag = "v1.3.0" }
+  }
+  , init = function ()
+    vim.g.compile_mode = {
+      baleia_setup = true
+    }
+  end
 }
