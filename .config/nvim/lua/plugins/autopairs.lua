62d1187ca27d91bb8d976cb4b37df1ddfc7b2de3
Vladimir Zdorenko <vovchikzd@gmail.com>
2025-08-17 14:30:56 +0300

    add pairs rule for zig's pipe
    #
    # Commands:
    #   q          Close
    #   <c-c><c-c> Submit
    #   <c-c><c-k> Abort
    #   <m-p>      Previous Message
    #   <m-n>      Next Message
    #   <m-r>      Reset Message

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -1,5 +1,24 @@
 return {
   "windwp/nvim-autopairs"
   , event = "InsertEnter"
-  , config = true
+  , config = function()
+    local Rule = require("nvim-autopairs.rule")
+    local cond = require("nvim-autopairs.conds")
+    local npairs = require("nvim-autopairs")
+    npairs.setup({})
+
+    npairs.add_rules({
+      Rule('|', '|', "zig")
+        :with_pair(cond.before_text(" catch "))
+        :with_move(cond.done())
+        :with_del(cond.done())
+        :with_cr(cond.none())
+      , Rule('|', '|', "zig")
+        :with_pair(cond.before_text(") "))
+        :with_move(cond.done())
+        :with_del(cond.done())
+        :with_cr(cond.none())
+    })
+
+  end
 }
