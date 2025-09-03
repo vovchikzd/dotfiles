e40966c73f828ea1892405c1df5b68a8637857b9
vovchikzd <vovchikzd@gmail.com>
2025-09-03 07:39:57 +0300

    change calculator app, comment btm since in always rebuild same version
    #
    # Commands:
    #   q          Close
    #   <c-c><c-c> Submit
    #   <c-c><c-k> Abort
    #   <m-p>      Previous Message
    #   <m-n>      Next Message
    #   <m-r>      Reset Message

--- .config/fish/functions/rsup.fish
+++ .config/fish/functions/rsup.fish
@@ -1,8 +1,9 @@
 function rsup --description 'Update rust and all apps'
   command rustup update; or return 1
-  set progs bat eva fd-find procs ripgrep sd starship tokei dua-cli zoxide\
-    difftastic bottom neocmakelsp yazi-fm yazi-cli eza tealdeer bacon mdbook\
+  set progs bat cpc fd-find procs ripgrep sd starship tokei dua-cli zoxide\
+    difftastic neocmakelsp yazi-fm yazi-cli eza tealdeer bacon mdbook\
     serie repgrep typst-cli
+  # bottom 
   for prog in $progs
     command cargo install $prog --locked; or return 1
   end
